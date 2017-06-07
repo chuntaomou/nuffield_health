72f8ef5403c6a7b8225237480a0555095c4d08e8
chuntaomou <chuntaomou@users.noreply.github.com>
2017-06-07 14:28:09 +0100

    dffff

--- routes/index.js
+++ routes/index.js
@@ -7,3 +7,34 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+//insert data into database
+var Connection=require('tedious').Connection;
+var Request=require('tedious').Request;
+
+var config={
+  userName:'ucabcm2',
+  password:'Mct.niub74110',
+  server:'http://nuffhealthproject.azurewebsites.net',
+  options:{
+    database:'nuffhealth_DB'
+  }
+}
+var connection=new Connection(config);
+connection.on('connect',function(err){
+  if(err){
+    console.log(err)
+  }
+  else{
+    insertIntoDatabase()
+  }
+});
+
+function insertIntoDatabase(){
+  console.log('insert row into databse');
+  request=new Request(
+    "insert into staff (staffName) values ('neeb')",
+    function(err){}
+  );
+  connection.exeSql(request);
+}
