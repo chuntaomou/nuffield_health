86510cf78865be12b25d25ac5b04a3046d1fb89d
chuntaomou <chuntaomou@users.noreply.github.com>
2017-06-07 15:25:24 +0100

    dfds

--- app.js
+++ app.js
@@ -42,5 +42,36 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+///////////////////////////////////////////////
+//insert data into database
+var Connection=require('tedious').Connection;
+var Request=require('tedious').Request;
 
+var config={
+  userName:'ucabcm2',
+  password:'Mct.niub74110',
+  server:'http://nuffhealthproject.azurewebsites.net',
+  options:{
+    database:'nuffhealth_DB'
+  }
+}
+var connection=new Connection(config);
+connection.on('connect',function(err){
+  if(err){
+    console.log(err)
+  }
+  else{
+    insertIntoDatabase()
+  }
+});
+
+function insertIntoDatabase(){
+  console.log('insert row into databse');
+  request=new Request(
+    "insert into staff (staffName) values ('neeb')",
+    function(err){}
+  );
+  connection.exeSql(request);
+}
+///////////////////////////////////////////////
 module.exports = app;
